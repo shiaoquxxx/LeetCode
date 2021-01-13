aef8f45bd74c4bc3eb82c80e00b27492f69f3c30
Shiao Qu <shiaoquxx.x@gmail.com>
2021-01-13 14:41:57 +0800

    Create 547. 省份数量.cpp

---
+++ 547. 省份数量.cpp
@@ -0,0 +1,42 @@
+// ----- ----- ----- ----- ----- ----- ----- -----
+// Created by Shiao Qu on 2021/1/7.
+// https://leetcode-cn.com/problems/number-of-provinces/
+// ----- ----- ----- ----- ----- ----- ----- -----
+
+#include "leetcode.h"
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>> &isConnected) {
+        vector<int> visited(isConnected.size(), 0);
+        int amount_of_provinces = 0;
+
+        for (int i = 0; i < isConnected.size(); ++i) {
+            if (dfs(isConnected, visited, i)) {
+                ++amount_of_provinces;
+            }
+        }
+
+        return amount_of_provinces;
+    }
+
+    bool dfs(vector<vector<int>> &g, vector<int> &visited, int city) {
+        if (visited[city]) {
+            return false;
+        }
+
+        visited[city] = 1;
+
+        for (int i = 0; i < g.size(); ++i) {
+            if (!g[city][i]) {
+                continue;
+            }
+
+            if (!visited[i]) {
+                dfs(g, visited, i);
+            }
+        }
+
+        return true;
+    }
+};
