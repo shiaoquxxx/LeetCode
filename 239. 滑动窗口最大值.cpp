7277f5f60ec10a7a744f4a3fb9b0d32cb49d39b4
Shiao Qu <shiaoquxx.x@gmail.com>
2021-01-13 16:28:06 +0800

    Update 239. 滑动窗口最大值.cpp

--- 239. 滑动窗口最大值.cpp
+++ 239. 滑动窗口最大值.cpp
@@ -8,30 +8,30 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int> &nums, int k) {
-        deque<pair<int, int>> mq;
+        deque<pair<int, int>> monotonic_queue;
         vector<int> maximum_elements;
 
         for (int i = 0; i < nums.size(); ++i) {
-            if (mq.empty()) {
-                mq.emplace_back(make_pair(nums[i], i));
+            if (monotonic_queue.empty()) {
+                monotonic_queue.emplace_back(make_pair(nums[i], i));
             }
             else {
-                if (mq.front().second <= i - k) {
-                    mq.pop_front();
+                if (monotonic_queue.front().second <= i - k) {
+                    monotonic_queue.pop_front();
                 }
 
-                if (mq.empty() || mq.back().first <= nums[i]) {
-                    while (!mq.empty() && mq.back().first <= nums[i]) {
-                        mq.pop_back();
+                if (monotonic_queue.empty() || monotonic_queue.back().first <= nums[i]) {
+                    while (!monotonic_queue.empty() && monotonic_queue.back().first <= nums[i]) {
+                        monotonic_queue.pop_back();
                     }
 
                 }
 
-                mq.emplace_back(make_pair(nums[i], i));
+                monotonic_queue.emplace_back(make_pair(nums[i], i));
             }
 
             if (i >= k - 1) {
-                maximum_elements.emplace_back(mq.front().first);
+                maximum_elements.emplace_back(monotonic_queue.front().first);
             }
         }
 
