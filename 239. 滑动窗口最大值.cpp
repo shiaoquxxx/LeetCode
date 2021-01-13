e9604aa8ae65c550906e57525cbbc16e6c095312
Shiao Qu <shiaoquxx.x@gmail.com>
2021-01-13 13:55:47 +0800

    Create 239. 滑动窗口最大值.cpp

---
+++ 239. 滑动窗口最大值.cpp
@@ -0,0 +1,40 @@
+// ----- ----- ----- ----- ----- ----- ----- -----
+// Created by Shiao Qu on 2021/1/2.
+// https://leetcode-cn.com/problems/sliding-window-maximum/
+// ----- ----- ----- ----- ----- ----- ----- -----
+
+#include "leetcode.h"
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int> &nums, int k) {
+        vector<int> maximum_elements;
+        deque<pair<int, int>> mq;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (mq.empty()) {
+                mq.emplace_back(make_pair(nums[i], i));
+            }
+            else {
+                if (mq.front().second <= i - k) {
+                    mq.pop_front();
+                }
+
+                if (mq.empty() || mq.back().first <= nums[i]) {
+                    while (!mq.empty() && mq.back().first <= nums[i]) {
+                        mq.pop_back();
+                    }
+
+                }
+
+                mq.emplace_back(make_pair(nums[i], i));
+            }
+
+            if (i >= k - 1) {
+                maximum_elements.push_back(mq.front().first);
+            }
+        }
+
+        return maximum_elements;
+    }
+};
