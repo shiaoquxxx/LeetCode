783adad31f4dc82cd9ec3d0d1ef19005a3f47927
Shiao Qu <shiaoquxx.x@gmail.com>
2021-01-13 16:28:11 +0800

    Update 684. 冗余连接.cpp

--- 684. 冗余连接.cpp
+++ 684. 冗余连接.cpp
@@ -8,17 +8,17 @@
 class Solution {
 public:
     vector<int> findRedundantConnection(vector<vector<int>> &edges) {
-        MergeFindSet mfs(edges.size() + 1); // LEETCODE.H
+        MergeFindSet merge_find_set(edges.size() + 1); // LEETCODE.H
 
         vector<int> redundant_connection;
 
         for (auto &edge: edges) {
-            if (mfs.find(edge[0]) == mfs.find(edge[1])) {
+            if (merge_find_set.find(edge[0]) == merge_find_set.find(edge[1])) {
                 redundant_connection = edge;
                 break;
             }
 
-            mfs.merge(edge[0], edge[1]);
+            merge_find_set.merge(edge[0], edge[1]);
         }
 
         return redundant_connection;
