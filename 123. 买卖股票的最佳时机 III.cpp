01fce8a838869f077f2e6601dbd296402c9ed395
Shiao Qu <shiaoquxx.x@gmail.com>
2021-01-13 15:49:55 +0800

    Update 123. 买卖股票的最佳时机 III.cpp

--- 123. 买卖股票的最佳时机 III.cpp
+++ 123. 买卖股票的最佳时机 III.cpp
@@ -8,18 +8,18 @@
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
-        int buy_once = -prices[0];
-        int sell_once = 0;
-        int buy_twice = -prices[0];
-        int sell_twice = 0;
+        int maximum_profit_by_buying_once = -prices[0];
+        int maximum_profit_by_selling_once = 0;
+        int maximum_profit_by_buying_twice = -prices[0];
+        int maximum_profit_by_selling_twice = 0;
 
         for (int i = 1; i < prices.size(); ++i) {
-            buy_once = max(buy_once, -prices[i]);
-            sell_once = max(sell_once, buy_once + prices[i]);
-            buy_twice = max(buy_twice, sell_once - prices[i]);
-            sell_twice = max(sell_twice, buy_twice + prices[i]);
+            maximum_profit_by_buying_once = max(maximum_profit_by_buying_once, -prices[i]);
+            maximum_profit_by_selling_once = max(maximum_profit_by_selling_once, maximum_profit_by_buying_once + prices[i]);
+            maximum_profit_by_buying_twice = max(maximum_profit_by_buying_twice, maximum_profit_by_selling_once - prices[i]);
+            maximum_profit_by_selling_twice = max(maximum_profit_by_selling_twice, maximum_profit_by_buying_twice + prices[i]);
         }
 
-        return sell_twice;
+        return maximum_profit_by_selling_twice;
     }
 };
