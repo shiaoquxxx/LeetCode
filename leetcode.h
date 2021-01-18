239bc956ad670e41d369f650cfb9b1316d926d00
Shiao Qu <shiaoquxx.x@gmail.com>
2021-01-18 12:11:43 +0800

    Update leetcode.h

--- leetcode.h
+++ leetcode.h
@@ -57,15 +57,11 @@ class MergeFindSet {
 private:
     vector<int> root;
     vector<int> rank;
-    vector<int> seen;
-    int amount_of_connected_components;
 
 public:
     explicit MergeFindSet(size_t n) {
         root.resize(n, 0);
         rank.resize(n, 1);
-        seen.resize(n, 0);
-        amount_of_connected_components = 0;
 
         for (int i = 0; i < n; ++i) {
             root[i] = i;
@@ -80,21 +76,9 @@ public:
         int _x = find(x);
         int _y = find(y);
 
-        if (!seen[x]) {
-            ++amount_of_connected_components;
-            seen[x] = 1;
-        }
-
-        if (!seen[y]) {
-            ++amount_of_connected_components;
-            seen[y] = 1;
-        }
-
         if (_x == _y) {
             return;
         }
-
-        --amount_of_connected_components;
 
         if (rank[_x] < rank[_y]) {
             swap(rank[_x], rank[_y]);
@@ -103,10 +87,6 @@ public:
         rank[_x] += rank[_y];
 
         root[_y] = _x;
-    }
-
-    int get_amount_of_connected_components() {
-        return amount_of_connected_components;
     }
 
     int get_rank(int x) {
