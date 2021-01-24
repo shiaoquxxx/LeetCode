ccff3bc54511daac7757b6ef0af5d64e93d5713d
Shiao Qu <shiaoquxx.x@gmail.com>
2021-01-24 08:16:58 +0800

    Create 674. 最长连续递增序列.cpp

---
+++ 674. 最长连续递增序列.cpp
@@ -0,0 +1,31 @@
+// ----- ----- ----- ----- ----- ----- ----- -----
+// Created by Shiao Qu on 2021/1/24.
+// https://leetcode-cn.com/problems/longest-continuous-increasing-subsequence/
+// ----- ----- ----- ----- ----- ----- ----- -----
+
+#include "leetcode.h"
+
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int> &nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+
+        int current_length_of_subsequence = 1;
+        int maximum_length_of_subsequence = 1;
+
+
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] > nums[i - 1]) {
+                ++current_length_of_subsequence;
+            }
+            else {
+                current_length_of_subsequence = 1;
+            }
+            maximum_length_of_subsequence = max(maximum_length_of_subsequence, current_length_of_subsequence);
+        }
+
+        return maximum_length_of_subsequence;
+    }
+};
