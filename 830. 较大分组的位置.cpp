6d8c6e43d49548f2342b5d39285bde867d68ab71
Shiao Qu <shiaoquxx.x@gmail.com>
2021-01-30 20:21:42 +0800

    Update 830. 较大分组的位置.cpp

--- 830. 较大分组的位置.cpp
+++ 830. 较大分组的位置.cpp
@@ -11,21 +11,21 @@ public:
         s += '#';
 
         vector<vector<int>> large_group_positions;
-        int start_of_group = 0;
-        int end_of_group = 0;
+        int starting_position_of_group = 0;
+        int ending_position_of_group = 0;
         char previous_character = '#';
 
         for (int i = 0; i < s.size(); ++i) {
             if (s[i] == previous_character) {
-                ++end_of_group;
+                ++ending_position_of_group;
             }
             else {
-                if (end_of_group - start_of_group + 1 >= 3) {
-                    large_group_positions.push_back({start_of_group, end_of_group});
+                if (ending_position_of_group - starting_position_of_group + 1 >= 3) {
+                    large_group_positions.push_back({starting_position_of_group, ending_position_of_group});
                 }
 
-                start_of_group = i;
-                end_of_group = i;
+                starting_position_of_group = i;
+                ending_position_of_group = i;
             }
 
             previous_character = s[i];
