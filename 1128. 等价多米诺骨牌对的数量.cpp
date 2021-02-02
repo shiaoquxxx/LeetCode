9d1ff8309fd58aec9143495ea3384805f74598d0
Shiao Qu <shiaoquxx.x@gmail.com>
2021-02-02 14:18:46 +0800

    Update 1128. 等价多米诺骨牌对的数量.cpp

--- 1128. 等价多米诺骨牌对的数量.cpp
+++ 1128. 等价多米诺骨牌对的数量.cpp
@@ -8,22 +8,22 @@
 class Solution {
 public:
     int numEquivDominoPairs(vector<vector<int>> &dominoes) {
-        array<int, 100> amount_of_dominoes;
+        array<int, 100> number_of_dominoes;
 
-        amount_of_dominoes.fill(0);
+        number_of_dominoes.fill(0);
 
         for (vector<int> &domino: dominoes) {
-            amount_of_dominoes[10 * min(domino[0], domino[1]) + max(domino[0], domino[1])]++;
+            number_of_dominoes[10 * min(domino[0], domino[1]) + max(domino[0], domino[1])]++;
         }
 
-        int amount_of_equivalent_dominoes = 0;
+        int number_of_equivalent_dominoes = 0;
 
-        for (int &amount_of_domino: amount_of_dominoes) {
-            if (amount_of_domino > 0) {
-                amount_of_equivalent_dominoes += amount_of_domino * (amount_of_domino - 1) / 2;
+        for (int &number_of_domino: number_of_dominoes) {
+            if (number_of_domino > 0) {
+                number_of_equivalent_dominoes += number_of_domino * (number_of_domino - 1) / 2;
             }
         }
 
-        return amount_of_equivalent_dominoes;
+        return number_of_equivalent_dominoes;
     }
 };
